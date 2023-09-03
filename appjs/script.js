e0f18113a45d68ad67948ecaec05885fdfa9eebc
Manuchehr <manuchehrsaidov998@gmail.com>
2023-09-03 22:42:08 +0500

    slider-mobile-full-commit

--- appjs/script.js
+++ appjs/script.js
@@ -1,16 +1,17 @@
-window.addEventListener('scroll', () => {
-  cards.forEach(card => {
-    const cardRect = card.getBoundingClientRect();
-    const sliderRect = slider.getBoundingClientRect();
-    const isVisible = (
-      cardRect.left >= sliderRect.left - 50 &&
-      cardRect.right <= sliderRect.right + 50
-    );
-
-    if (isVisible) {
-      card.classList.add('center');
-    } else {
-      card.classList.remove('center');
-    }
-  });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  zoom: {
+    maxRatio: 5,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
 });
